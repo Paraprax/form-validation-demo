f2638110d6c4e36292e9de0eed3246eb4210492d
Paraprax <parkerjohnston28@gmail.com>
2024-12-10 07:13:37 -0500

    Add note comment re: number formats of the major credit cards

--- 2-payment-validation.js
+++ 2-payment-validation.js
@@ -8,4 +8,8 @@ document.addEventListener("DOMContentLoaded", () => {
   // TODO: validate CCN format
   // TODO: check CVV length
   // TODO: prevent submission of invalid or incomplete payment info
+  /* NOTE: Visa cards begin with a 4 && have 13 or 16 digits. 
+  Mastercard cards begin w/ a 5 && have 16 digits. Amex begin with a 3, 
+  followed by a 4 or a 7 and have 15 digits. Discover cards begin with a 
+  6 and have 16 digits. */
 });
