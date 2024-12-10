2f08cf8c74e95586fe3798c9c2afbb87c0cea3c9
Paraprax <parkerjohnston28@gmail.com>
2024-12-10 12:43:43 -0500

    Retool date validation logic && remove debugging console logs

--- 2-payment-validation.js
+++ 2-payment-validation.js
@@ -24,16 +24,10 @@ document.addEventListener("DOMContentLoaded", () => {
     const userMonthAndYear = userDate.split("/");
     const userMonth = parseInt(userMonthAndYear[0]);
     const userYear = parseInt(userMonthAndYear[1]);
-    console.log(thisMonth);
-    console.log(thisYear);
-    console.log(userMonth);
-    console.log(userYear);
 
     return (
-      userYear.length == 2 &&
-      userYear >= thisYear &&
-      userMonth.length == 2 &&
-      userMonth >= thisMonth
+      (userYear <= 99 && userYear > thisYear && userMonth <= 12) ||
+      (userYear === thisYear && userMonth <= 12 && userMonth >= thisMonth)
     );
   };
 
@@ -51,16 +45,13 @@ document.addEventListener("DOMContentLoaded", () => {
     const isValidNumber = validateCCNumber(ccNumber);
     const isValidExpDate = validateExpirationDate(ccDate);
     const isValidCVV = validateCVVNumber(ccCVV);
-    console.log(isValidNumber);
-    console.log(isValidExpDate);
-    console.log(isValidCVV);
 
     if (isValidNumber && isValidExpDate && isValidCVV) {
       ccForm.submit();
     } else if (!isValidNumber) {
       alert("Number must be 13 or 16 digits long and begin with a 4");
     } else if (!isValidExpDate) {
-      alert("Expiration date must not be in the past");
+      alert("Invalid expiration date entered");
     } else if (!isValidCVV) {
       alert("CVV must be 3 digits long");
     }
