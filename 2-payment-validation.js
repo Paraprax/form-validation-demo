3e7f6e7b6738a3344e7ca209c100c4ec10a583b0
Paraprax <parkerjohnston28@gmail.com>
2024-12-12 12:19:01 -0500

    Write switch-case for card type validiation conditions && clean up card type variable setting

--- 2-payment-validation.js
+++ 2-payment-validation.js
@@ -1,27 +1,51 @@
 document.addEventListener("DOMContentLoaded", () => {
   const ccForm = document.getElementById("cc-form");
-  const cardType = document.getElementsByName("card-type");
+  const cardTypeInput = document.getElementsByName("card-type");
   const ccNumberInput = document.getElementById("cc-number");
   const ccExpirationInput = document.getElementById("expiration");
   const ccCVVinput = document.getElementById("cvv");
 
   // card type selection function:
   const getCardType = () => {
-    for (i = 0; i < cardType.length; i++) {
-      if (cardType[i].checked) console.log(cardType[i].value);
+    for (i = 0; i < cardTypeInput.length; i++) {
+      if (cardTypeInput[i].checked) cardType = cardTypeInput[i].value;
     }
+    return cardType;
   };
 
   // number validation function:
-  const validateCCNumber = (number) => {
-    const validLengths = [13, 16];
-    const validStart = "4";
+  const validateCCNumber = (cardType, number) => {
+    /* NOTE: Visa cards begin with a 4 && have 13 or 16 digits. Mastercard cards begin 
+    w/ a 5 && have 16 digits. Amex begin with a 3 and have 15 digits. Discover cards begin 
+    with a 6 and have 16 digits. */
+    const validLengths = [13, 15, 16];
+    const validStarts = ["3", "4", "5", "6"];
+    const cardTypeOptions = ["visa", "mastercard", "amex", "discover"];
     const splitNumber = number.toString().split("");
 
-    return (
-      splitNumber.length === validLengths[0] ||
-      (splitNumber.length === validLengths[1] && splitNumber[0] == validStart)
-    );
+    switch (cardType) {
+      case cardTypeOptions[0]: //visa
+        return (
+          splitNumber.length === validLengths[0] ||
+          (splitNumber.length === validLengths[1] &&
+            splitNumber[0] == validStarts[1])
+        );
+      case cardTypeOptions[1]: //mastercard
+        return (
+          splitNumber.length === validLengths[1] &&
+          splitNumber[0] == validStarts[2]
+        );
+      case cardTypeOptions[2]: //amex
+        return (
+          splitNumber.length === validLengths[1] &&
+          splitNumber[0] == validStarts[0]
+        );
+      case cardTypeOptions[3]: //discover
+        return (
+          splitNumber.length === validLengths[2] &&
+          splitNumber[0] == validStarts[3]
+        );
+    }
   };
 
   // exp. date validation function:
@@ -45,11 +69,12 @@ document.addEventListener("DOMContentLoaded", () => {
 
   ccForm.addEventListener("submit", (event) => {
     event.preventDefault();
-    getCardType();
+    const ccType = getCardType();
+    console.log(ccType);
     const ccNumber = ccNumberInput.value.trim();
     const ccDate = ccExpirationInput.value.trim();
     const ccCVV = ccCVVinput.value.trim();
-    const isValidNumber = validateCCNumber(ccNumber);
+    const isValidNumber = validateCCNumber(ccType, ccNumber);
     const isValidExpDate = validateExpirationDate(ccDate);
     const isValidCVV = validateCVVNumber(ccCVV);
 
